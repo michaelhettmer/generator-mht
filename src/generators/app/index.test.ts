6c4b7c07b3ed03ee3b2584d99dbba9a4ee6ed891
Michael Hettmer <mail@michael-hettmer.de>
2020-02-14 21:55:50 +0100

    feat: improved base generator test case

--- src/generators/app/index.test.ts
+++ src/generators/app/index.test.ts
@@ -1,11 +1,10 @@
-import Generator from './index';
 import helpers from 'yeoman-test';
+import assert from 'yeoman-assert';
+import Generator from './index';
 
 describe('mht:app', () => {
-    it('successfully starts the generator and returns without an error', () => {
-        return helpers
-            .run(Generator)
-            .withPrompts({ generator: 'latex' })
-            .then(() => console.log('finished successfully'));
+    it('successfully starts the generator and returns without an error', async () => {
+        const result = await helpers.run(Generator).withPrompts({ generator: 'latex' });
+        assert.ok(result, 'successfully started');
     });
 });
