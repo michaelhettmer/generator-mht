d7ded99127c09582bce3491dfa7a4f384a22589d
Michael Hettmer <mail@michael-hettmer.de>
2020-02-15 15:28:46 +0100

    build: ignore template files in jest module path

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    modulePathIgnorePatterns: ['<rootDir>/.*/templates'],
     setupFilesAfterEnv: ['jest-mock-console/dist/setupTestFramework.js'],
     testEnvironment: 'node',
 };
